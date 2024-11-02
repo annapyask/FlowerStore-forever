da7b29d7d4193cc9dbbd7e1fe949f3fd87230bc4
Anna Piaskovska <piaskovska.pn@ucu.edu.ua>
2024-11-02 21:15:48 +0200

    aplication

---
+++ src/main/java/ucu/edu/ua/appps/flower_lab8/FlowerLab8Application.java
@@ -0,0 +1,13 @@
+package ucu.edu.ua.appps.flower_lab8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerLab8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerLab8Application.class, args);
+	}
+
+}
