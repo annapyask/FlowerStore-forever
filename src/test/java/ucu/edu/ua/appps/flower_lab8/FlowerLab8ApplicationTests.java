5bf32d0b8ed6db33e5aeef22a61ff84e30ee34a8
Anna Piaskovska <piaskovska.pn@ucu.edu.ua>
2024-11-02 21:17:58 +0200

    aplication

---
+++ src/test/java/ucu/edu/ua/appps/flower_lab8/FlowerLab8ApplicationTests.java
@@ -0,0 +1,13 @@
+package ucu.edu.ua.appps.flower_lab8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerLab8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
